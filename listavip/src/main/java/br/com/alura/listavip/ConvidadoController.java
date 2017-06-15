b0ab59540ad69e6c808063e1b2c9d5a7dfa2f3c2
BsdCode <bruno.sdelgado@outlook.com>
2017-06-15 13:53:15 -0300

    Configuração do DataSource e criação Entity Convidado

--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -2,7 +2,6 @@ package br.com.alura.listavip;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ConvidadoController {
